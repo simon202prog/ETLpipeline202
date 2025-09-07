9f80bef8c2047e0b205ec561c002f2af246ff341
Simon <simon202prog@gmail.com>
2025-09-07 18:31:42 +0300

    Add right load to valid json

--- dags/load_row_from_api_to_s3_v1.py
+++ dags/load_row_from_api_to_s3_v1.py
@@ -4,6 +4,8 @@ from airflow.operators.python import PythonOperator
 import requests
 from datetime import timedelta, datetime
 import boto3
+import json
+
 def get_query_for_day(**context):
     exec_date = context["execution_date"]
     start_date = (exec_date - timedelta(days=1)).strftime("%Y-%m-%d")
@@ -16,32 +18,38 @@ def get_query_for_day(**context):
         "endtime": end_date,
     }
 
+    response = requests.get(url=url, params=params, timeout=60)
+    response.raise_for_status()
+    data = response.json() 
+
+    json_bytes = json.dumps(data).encode("utf-8")
+
     s3 = boto3.client(
         "s3",
-        aws_access_key_id = Variable.get("AWS_ACCESS_KEY_ID"),
+        aws_access_key_id=Variable.get("AWS_ACCESS_KEY_ID"),
         aws_secret_access_key=Variable.get("AWS_SECRET_ACCESS_KEY"),
         region_name="us-east-1",
         endpoint_url="http://minio:9000"
     )
+
     bucket = "raw-layer"
-    key = key = f"earthquake/{(exec_date - timedelta(days=1)).strftime('%Y/%m/%d')}.json"
-    with requests.get(url=url, params=params, stream=True, timeout=60) as response:
-        s3.upload_fileobj(response.raw, bucket, key)
+    key = f"earthquake/{(exec_date - timedelta(days=1)).strftime('%Y/%m/%d')}.json"
+
+    s3.put_object(Bucket=bucket, Key=key, Body=json_bytes)  
 
 default_args = {
-    "owner":"admin",
+    "owner": "admin",
 }
 
 with DAG(
-    dag_id = "load_row_from_api_to_s3",
+    dag_id="load_row_from_api_to_s3",
     default_args=default_args,
-    start_date = datetime(2025,9,6),
-    schedule_interval = '@daily',
+    start_date=datetime(2025, 9, 6),
+    schedule_interval='@daily',
     catchup=False
 ) as dag:
     task_1 = PythonOperator(
-        task_id = "from_api_to_s3",
-        python_callable = get_query_for_day
+        task_id="from_api_to_s3",
+        python_callable=get_query_for_day
     )
     task_1
-
